0d7f07285b62eea08a874ae159c34ebb5cad85d9
Olekwer <olekwer@gmail.com>
2016-10-17 16:06:04 +0300

    task4

--- task4/src/com/example/oleg/controller/ServeceList.java
+++ task4/src/com/example/oleg/controller/ServeceList.java
@@ -6,15 +6,23 @@ import java.util.List;
 import com.example.oleg.model.Service;
 
 public class ServeceList implements Lists {
-	
+
 	private List<Service> servicesList;
-	
+
 	public void creat() {
-		servicesList=new ArrayList<Service>();
+		servicesList = new ArrayList<Service>();
 		for (int i = 0; i < 5; i++) {
-			
+			Service service = new Service();
+			int id = i + 1;
+			String name = "service" + i;
+			double price = Math.floor(Math.random() * 1000);
+			service.setId(id);
+			service.setName(name);
+			service.setPrice(price);
+			servicesList.add(service);
 		}
 	}
+
 	public List<Service> getList() {
 		return this.servicesList;
 	}
