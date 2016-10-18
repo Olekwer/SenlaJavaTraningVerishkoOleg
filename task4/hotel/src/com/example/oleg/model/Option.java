01b6cc192f0089e55339512b5f323346ac98e173
Oleg Verishko <olekwer@gmail.com>
2016-10-19 01:56:49 +0300

    hotel

--- task4/hotel/src/com/example/oleg/model/Option.java
+++ task4/hotel/src/com/example/oleg/model/Option.java
@@ -1,5 +1,43 @@
 package com.example.oleg.model;
 
-public class Option {
+import java.util.Date;
 
+public class Option {
+	private int id;
+	private String name;
+	private double price;
+	private Date dateExecut;
+	public Option(int id, String name, double price){
+		this.id=id;
+		this.name=name;
+		this.price=price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDateExecut() {
+		return dateExecut;
+	}
+	public void setDateExecut(Date dateExecut) {
+		this.dateExecut = dateExecut;
+	}
+	public String toString(){
+		String s=this.name+" "+this.price;
+		return s;
+	}
 }
--- task4/hotel/src/com/example/oleg/model/Room.java
+++ task4/hotel/src/com/example/oleg/model/Room.java
@@ -1,5 +1,54 @@
 package com.example.oleg.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+	private int number;
+	private double price;
+	private int capacity;
+	private int stars;
+	private List<Guest> guest;
+	public Room(int number, double price,int capacity,int stars){
+		this.number=number;
+		this.price=price;
+		this.capacity=capacity;
+		this.stars=stars;
+		this.guest= new ArrayList<Guest>();
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+	public List<Guest> getGuest() {
+		return guest;
+	}
+	public void setGuest(List<Guest> guest) {
+		this.guest = guest;
+	}
+	public String toString(){
+		String s=this.number+" "+this.price+" "+this.capacity+" "+this.stars;
+		return s;
+	}
 
 }
