52cbdfa81293c5e3a603e1180d6f3e5aedf29135
Oleg Verishko <olekwer@gmail.com>
2016-10-24 23:37:13 +0300

    update

---
+++ task4/hotel/src/com/example/oleg/compare/room/CompareRoomCapacity.java
@@ -0,0 +1,22 @@
+package com.example.oleg.compare.room;
+
+import java.util.Comparator;
+
+import com.example.oleg.model.Room;
+
+public class CompareRoomCapacity implements Comparator<Room> {
+
+	@Override
+	public int compare(Room arg0, Room arg1) {
+		int capacity1 = arg0.getCapacity();
+		int capacity2 = arg1.getCapacity();
+		if (capacity1 > capacity2) {
+			return 1;
+		} else if (capacity1 < capacity2) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+
+}
---
+++ task4/hotel/src/com/example/oleg/model/Room.java
@@ -0,0 +1,76 @@
+package com.example.oleg.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+	private int number;
+	private double price;
+	private int capacity;
+	private int stars;
+	private StatusRoom statusRoom;
+	private List<Guest> guest;
+
+	public Room(int number, double price, int capacity, int stars) {
+		this.number = number;
+		this.price = price;
+		this.capacity = capacity;
+		this.stars = stars;
+		this.guest = new ArrayList<Guest>();
+		this.setStatusRoom(StatusRoom.REPAIRS);
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public int getStars() {
+		return stars;
+	}
+
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+
+	public List<Guest> getGuest() {
+		return guest;
+	}
+
+	public void setGuest(List<Guest> guest) {
+		this.guest = guest;
+	}
+
+	public String toString() {
+		String s = this.number + " " + this.price + " " + this.capacity + " " + this.stars;
+		return s;
+	}
+
+	public StatusRoom getStatusRoom() {
+		return statusRoom;
+	}
+
+	public void setStatusRoom(StatusRoom statusRoom) {
+		this.statusRoom = statusRoom;
+	}
+
+}
